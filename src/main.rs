8dfd6197cd10064b3e35d14a549a875d742839b1
Ryan Wendell <ryanjwendell@gmail.com>
2023-01-18 20:07:43 -0600

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,33 @@
+fn main() {
+    use gilrs::{Button, Event, Gilrs};
+
+    let mut gilrs = Gilrs::new().unwrap();
+
+    // Iterate over all connected gamepads
+    for (_id, gamepad) in gilrs.gamepads() {
+        println!("{} is {:?}", gamepad.name(), gamepad.power_info());
+    }
+
+    let mut active_gamepad = None;
+
+    loop {
+        // Examine new events
+        while let Some(Event { id, event, time }) = gilrs.next_event() {
+            println!("{:?} New event from {}: {:?}", time, id, event);
+            active_gamepad = Some(id);
+        }
+
+        // You can also use cached gamepad state
+        if let Some(gamepad) = active_gamepad.map(|id| gilrs.gamepad(id)) {
+            if gamepad.is_pressed(Button::South) {
+                println!("Button South is pressed (XBox - A, PS - X)");
+            }
+        }
+    }
+}
+
+
+//Steps for this project:
+
+// Connect each controller: pairing them -> add to vector
+// send signals to switch using this PR https://github.com/JonnyHaystack/HayBox/pull/20 or maybe just grab the descriptors directly off of https://github.com/progmem/Switch-Fightstick 
